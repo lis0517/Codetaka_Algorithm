330244e62305e1b5822aac5489b8cdb2c58a658f
lis0517 <43354156+lis0517@users.noreply.github.com>
2024-04-26 10:17:59 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        if ( n < 2 )
+            return 0;
+        
+        int answer = 0;
+        for ( int i = 2; i <= n; i += 2 ){
+            answer += i;
+        }
+        return answer;
+    }
+}
