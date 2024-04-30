68574e72f4d95dcca3c49939d0e5b70b68862cd6
lis0517 <43354156+lis0517@users.noreply.github.com>
2024-04-30 09:43:10 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,14 +1,8 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        for ( int i = 0; i < absolutes.length; ++i )
-        {
-            if (!signs[i])
-                absolutes[i] *= -1;
-        }
-        
-        for(int value : absolutes){
-            answer += value;
+        for ( int i = 0; i < absolutes.length; ++i ) {
+            answer += !signs[i] ? absolutes[i] * -1 : absolutes[i];
         }
         return answer;
     }
