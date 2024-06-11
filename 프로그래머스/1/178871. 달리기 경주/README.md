3f19fd875fabac2ff7e94b15d7dc0c3f0414c484
lis0517 <43354156+lis0517@users.noreply.github.com>
2024-06-11 09:32:47 +0900

    [level 1] Title: 달리기 경주, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=java) 
 
 ### 성능 요약
 
-메모리: 81 MB, 시간: 0.04 ms
+메모리: 76.2 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 10일 09:47:33
+2024년 06월 11일 09:32:46
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,22 +1,30 @@
 import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        HashMap<String, Integer> playerRank = new HashMap<>();
+        int n = players.length;
+        Map<String, Integer> playerMap = new HashMap<>();
 
-        for (int i = 0; i < players.length; i++) {
-            playerRank.put(players[i], i);
+        // 선수 이름과 등수를 매핑
+        for (int i = 0; i < n; i++) {
+            playerMap.put(players[i], i);
         }
 
-        for (String calledPlayer : callings) {
-            int calledRank = playerRank.get(calledPlayer);
-            String frontPlayer = players[calledRank - 1];
+        for (String calling : callings) {
+            int currentRank = playerMap.get(calling);
 
-            players[calledRank - 1] = calledPlayer;
-            players[calledRank] = frontPlayer;
+            if (currentRank > 0) {
+                String previousPlayer = players[currentRank - 1];
 
-            playerRank.put(calledPlayer, calledRank - 1);
-            playerRank.put(frontPlayer, calledRank);
+                // 선수 이름 배열 업데이트
+                players[currentRank - 1] = calling;
+                players[currentRank] = previousPlayer;
+
+                // 선수 이름과 등수 매핑 업데이트
+                playerMap.put(calling, currentRank - 1);
+                playerMap.put(previousPlayer, currentRank);
+            }
         }
 
         return players;
